46cb5eaaebc6d37bc5aacfa1467d5f09b93e6f7c
Ichiro Okazaki <okazaki@heartrails.com>
2015-11-22 10:56:15 +0900

    add toggleChat function.

--- js/SpaShell.ts
+++ js/SpaShell.ts
@@ -18,26 +18,56 @@ export class SpaShell {
 		<div class="spa-shell-foot"></div>
 		<div class="spa-shell-chat"></div>
 		<div class="spa-shell-modal"></div>
-		`};
-	stateMap = { $container: null };
-	jqueryMap = {};
+		`,
+		chat_extend_time: 1000,
+		chat_retract_time: 300,
+		chat_extedn_height: 450,
+		chat_retract_height: 15
+	};
+	private stateMap: { $container?: JQuery } = {};
+	private jqueryMap: { $container?: JQuery, $chat?: JQuery } = {};
 		
 	/**
 	 * コンストラクタ
 	 */
-	constructor($container :JQuery) {
+	constructor($container: JQuery) {
 		this.initModule($container)
 	}
 
 	private setJqueryMap(): void {
 		var $container = this.stateMap.$container;
-		this.jqueryMap = { $contaier: $container };
+		this.jqueryMap.$container = $container;
+		this.jqueryMap.$chat = $container.find('.spa-shell-chat');
 	}
 
 	private initModule($container: JQuery): void {
 		this.stateMap.$container = $container;
 		$container.html(this.configMap.main_html);
 		this.setJqueryMap();
+	}
+
+	public toggleChat(do_extend: boolean, callback?: Function): boolean {
+		var px_chat_ht = this.jqueryMap.$chat.height();
+		var is_open = px_chat_ht == this.configMap.chat_extedn_height;
+		var is_closed = px_chat_ht == this.configMap.chat_retract_height;
+		var is_sliding = !is_open && !is_closed;
+		
+		if (is_sliding) return false;
+
+		if (do_extend) {
+			this.jqueryMap.$chat.animate(
+				{ height: this.configMap.chat_extedn_height },
+				this.configMap.chat_extend_time,
+				() => { if (callback) { callback(this.jqueryMap.$chat); } }
+			);
+			return true;
+		}
+		this.jqueryMap.$chat.animate(
+			{ height: this.configMap.chat_retract_height },
+			this.configMap.chat_retract_time,
+			() => { if (callback) { callback(this.jqueryMap.$chat); } }
+		);
+		return true;
 	}
 
 }
